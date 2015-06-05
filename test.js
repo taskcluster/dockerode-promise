8d52ba1afa66d9d7c33d573cad4f9f4959559a6d
Jonas Finnemann Jensen <jopsen@gmail.com>
2015-06-04 17:35:32 -0700

    Added some test

--- test.js
+++ test.js
@@ -1,10 +1,29 @@
 suite("dockerode-promise", function() {
   var Docker = require('./');
+  var assert = require('assert');
+
+  var docker = new Docker({
+    socketPath: '/var/run/docker.sock'
+  });
 
   test("docker.info", function() {
-    var docker = new Docker({
-      socketPath: '/var/run/docker.sock'
-    });
     return docker.info();
+  });
+
+  test("docker.createContainer", function() {
+    return docker.createContainer({
+      Image: 'busybox',
+      Command: ['true'],
+      AttachStdin: false,
+      AttachStdout: false,
+      AttachStderr: false
+    }).then(function(container) {
+      assert(container.id);
+      return container.start().then(function() {
+        return container.wait();
+      }).then(function() {
+        return container.remove({force: true, v: true});
+      });
+    })
   });
 });
